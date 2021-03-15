25c6b362abb82df95721cb563fdd9c5e5c6df547
Assiab2707 <assiab2707@yahoo.com>
2021-03-15 22:42:52 +0100

    add function format_date

--- biketrauma/preprocess/format_date.py
+++ biketrauma/preprocess/format_date.py
@@ -1,2 +1,4 @@
 def format_date(df_bikes):
-    print(df_bikes)
+    df = df_bikes.drop(columns = ['date', 'mois','jour','heure'])
+    df.insert(0,"date_formated",df_bikes['date']+"T"+df_bikes['heure']+":00")
+    return df
--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 import biketrauma
 
 df = biketrauma.Load_db().save_as_df()
-biketrauma.plot_location(biketrauma.get_accident(df))
+df_nicely_formated = biketrauma.format_date(df)
+#biketrauma.plot_location(biketrauma.get_accident(df))
+
+
