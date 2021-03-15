d2bf96e89690c2196bec64f5eb5fa995e78990d3
Assiab2707 <assiab2707@yahoo.com>
2021-03-15 23:07:53 +0100

    remove comments

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ import biketrauma
 
 df = biketrauma.Load_db().save_as_df()
 df_nicely_formated = biketrauma.format_date(df)
-#biketrauma.plot_location(biketrauma.get_accident(df))
+biketrauma.plot_location(biketrauma.get_accident(df))
 
 
